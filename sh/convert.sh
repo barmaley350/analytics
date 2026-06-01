cb054d83d0137ed2e5e1e14bee63db9babc4b766
Andrey Frolov <darmaley40@yandex.ru>
2026-06-02 01:20:40 +0300

    Refactoring

--- sh/convert.sh
+++ sh/convert.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
-dataset_cars="../datasets/raw_data/cars/cars_sales.csv"
-dataset_liquor="../datasets/raw_data/liquor/Liquor_Sales.csv"
+datasets=("./datasets/raw_data/cars/cars_sales.csv" "./datasets/raw_data/liquor/Liquor_Sales.csv")
+
 
 ParquetCompression=("brotli" "gzip" "none" "zstd" "lz4" "snappy")
 
-for Compression in "${ParquetCompression[@]}"
-do
-    echo "Convert cars_sales.csv to parquet with $Compression compression"
-    ./clickhouse local --max_memory_usage=2G --max_threads=1 --output_format_parquet_compression=$Compression -q "SELECT * FROM file ('./datasets/raw_data/cars/cars_sales.csv', CSVWithNames) INTO OUTFILE './datasets/raw_data/cars/cars_sales_$Compression.parquet' FORMAT Parquet"
+for dataset in "${datasets[@]}"; do
+    dataset_path=$(dirname "$dataset")
+    dataset_name=$(basename "$dataset")
+    dataset_name_template="${dataset_name%.*}"
+   
+    for Compression in "${ParquetCompression[@]}"; do
+        echo "Convert $dataset_name to parquet with $Compression compression"
+        ./clickhouse local \
+            --max_memory_usage=2G \
+            --max_threads=1 \
+            --output_format_parquet_compression="$Compression" \
+            -q "SELECT * FROM file('$dataset', 'CSVWithNames') \
+            INTO OUTFILE '${dataset_path}/${dataset_name_template}_${Compression}.parquet' FORMAT Parquet"
+    done
 done
